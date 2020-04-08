2529e504003047ce6bd9ae6d845b1499ebf6067e
Brendan Broadstreet <brendanbroadstreet@Brendans-Air.attlocal.net>
2020-04-07 20:17:17 -0400

    cart changes

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 import { environment } from 'src/environments/environment'
 
-export const baseUrl = environment.production ? 'https://api.shoppingcart.com' : 'http://localhost:3000'
-export const productsUrl = baseUrl + '/products';
-export const cartUrl = baseUrl + '/cart';
+export const baseUrl = environment.production ? 'http://localhost:4000' : 'http://localhost:4000'
+export const productsUrl = baseUrl + '/api/menu/myrecipes';
+export const cartUrl = baseUrl + '/api/cart';
 export const favlistUrl = baseUrl + '/favlist'
